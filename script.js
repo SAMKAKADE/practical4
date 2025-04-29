a68304786fb431b1f261b1ee4b0b02d5135dea40
Samarth kakade <samarthkakade11@gmail.com>
2025-04-29 11:50:00 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const validUsername = 'samarthkakade';
+const validPassword = '12345678';
+
+document.getElementById('loginForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const enteredUsername = document.getElementById('username').value;
+    const enteredPassword = document.getElementById('password').value;
+
+    if (enteredUsername === validUsername && enteredPassword === validPassword) {
+        alert('Login successful!');
+    } else {
+        const errorMessage = document.getElementById('errorMessage');
+        errorMessage.style.display = 'block';
+    }
+});
